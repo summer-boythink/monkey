0fe5e173bd3d8f78c9615a8d90334eb02afe9bf6
summer-boythink <2071792205@qq.com>
2023-03-20 23:35:03 +0800

    begin ast

---
+++ src/ast/ast.rs
@@ -0,0 +1,27 @@
+pub trait Node {
+    fn token_literal(&self) -> String;
+}
+
+pub trait Statement: Node {
+    fn statement_node(&self);
+}
+
+pub trait Expression: Node {
+    fn expression_node(&self);
+}
+
+pub struct Program {
+    statements:Vec<Box<dyn Statement>>
+}
+
+impl Program {
+    pub fn token_literal(&self) -> String {
+        if self.statements.len() > 0 {
+            self.statements[0].token_literal()
+        }else {
+            "".to_string()
+        }
+    }
+
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+pub mod ast;
 pub mod lexer;
 pub mod repl;
 pub mod token;
 
+pub use ast::*;
 pub use lexer::*;
 pub use repl::*;
 pub use token::*;
